c46764daecf8f4a4e17f2f42cf17d285bee40feb
topYoung <103985664+topYoung@users.noreply.github.com>
2024-08-14 09:55:36 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -617,8 +617,8 @@ function changeValue() {
                 colName = data.ColName
                 console.log('rowName=', rowName)
                 console.log('colName=', colName)
-                rowName = '物料B'
-                colName = 'Status'
+                // rowName = '物料B'
+                // colName = 'Status'
                 const tmp = allData.boards[0].columns
                 for (let j = 0; j < tmp.length; j++) {
                     if (tmp[j].title == colName) {
