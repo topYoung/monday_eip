e9d81a197b777e0872ed7d29b139582e173ff712
topYoung <103985664+topYoung@users.noreply.github.com>
2024-08-21 16:31:36 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -683,6 +683,11 @@ function changeValue() {
                                 .then(res => res.json())
                                 .then(res => {
                                     console.log(JSON.stringify(res, null, 2))
+                                    let json = JSON.stringify(res, null, 2)
+                                    let  error_message = ''
+                                    error_message = json['error_message']
+                                    console.log('error_message = ',error_message)
+                                    // if(error_message == '')
                                     info.innerHTML = "此案A3已重置成功，請重新至EIP執行A3簽核流程"
                                     info.style.fontSize = "18px"
                                 });
